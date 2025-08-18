330e5da21a7dfb4a39b092a6f7134c9793ea862f
joselio reis de oliveira <joseliooliver982@gmail.com>
2025-08-18 12:00:19 -0300

    Update CartasSuperTrunfo.c

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,47 +1,93 @@
 #include <stdio.h>
 
-int main(){
+int main() {
+    char cidade[50];
+    char estado[50];
+    int população;
+    float area_km2;
+    float pib;
+    int ponto_turistico;
+    char codigo_cartas[3];
 
-int Estado[50];
-int Cidade[50];
-char Condigo_carta[3];
-int Populacão;
-float Area_km²;
+    printf("digite o nome da sua cidade: \n");
+    scanf("%s", &cidade);
+    printf("o nome da cidade e: %s\n", cidade);
+
+    printf("digite o nome do estado: \n");
+    scanf("%s", &estado);
+    printf("o nome do estado e: %s\n", estado);
+
+    printf("digite numero de população: \n");
+    scanf("%d", &população);
+    printf("numero de populacao e: %d\n", população);
+
+    printf("digite o valor do km2: \n");
+    scanf("%f", &area_km2);
+    printf("area em km2: %.2f\n", area_km2);
+
+    printf("digite o valor do pib:(em bilhões de reais): \n");
+    scanf("%f", &pib);
+    printf("pib; %.2f bilhões de reais: \n", pib);
+
+
+    printf("digite o numero de pontos turisticos: \n");
+    scanf("%d", &ponto_turistico);
+    printf("numero de pontos turisticos e: %d\n", ponto_turistico);
+
+    printf("digite o codigo das cartas: \n");
+    scanf("%s", &codigo_cartas);
+    printf("o codigo das cartas e: %s\n", codigo_cartas);
+
+    printf("A cidade %s, localizada no estado %s, possui uma população de %d habitantes, uma área de %.2f km² e um PIB de %.2f bilhões de reais. Com %d pontos turísticos, o código das cartas é %s.\n", 
+           cidade, estado, população, area_km2, pib, ponto_turistico, codigo_cartas);
+
+
+
+
+
+printf("Aqui está a carta dois:\n");
+
+char Cidade[50];
+char Estado[50];
+char Codigo_carta[4];
+int Populacao;
+float Area_km2;
 float Pib;
 int Pontos_turisticos;
 
-printf("Digite o nome do estado:\n");
-scanf("%s", &Estado);
-printf("nome do Estado é: %s\n", Estado);
 
-printf("Digite o nome da cidade:\n");
-scanf("%s", &Cidade);
-printf("O nome da ciade é: %s\n", Cidade);
+printf("Digite sua cidade:\n");
+scanf("%s",Cidade);
+printf("Sua cidade é: %s\n", Cidade);
 
-printf("Código da carta:\n");
-scanf("%s", &Condigo_carta);
-printf("O condigo da carta é: %s\n", Condigo_carta);
+printf("Digite seu estado:\n");
+scanf("%s", Estado);
+printf("seu estado é: %s\n", Estado);
 
-printf("Numeros de habitantes:\n");
-scan("%d", &Populacão);
-printf("Habitantes total é: %d\n", Populacão);
+printf("Digite o codigo da carta:\n");
+scanf("%s", &Codigo_carta);
+printf("O codigo da carta é: %s\n", Codigo_carta);
 
-printf("Quantos quilomentros km²?\n");
-scanf("%2.f", &Area_km²);
-printf("Quilomentros km² %2.f\n", Area_km²);
+printf("Populacão atual é:\n");
+scanf("%d", &Populacao);
+printf("A populacao atual é: %d\n", Populacao);
 
-printf("Digite o Pib\n");
-scanf("%2.f", &Pib);
-printf("Numero do Pib é: %.2f\n", Pib);
+printf("Digite Área em km²:\n");
+scanf("%f", &Area_km2);
+printf("Area em km² é: %.2f\n", Area_km2);
 
-printf("Quantos pontos turisticos?\n");
-scanf("%d", &Pontos_turisticos);
-printf("Totais de pontos são: %d\n", Pontos_turisticos);
+ printf("Digite o PIB:\n");
+scanf("%f", &Pib);
+    printf("O valor do Pib é: %.2f\n", Pib);
 
+    printf("Digite quantos pontos turisticos: \n");
+    scanf("%d", &Pontos_turisticos);
+    printf("Numeros de pontos turistico é: %d\n", Pontos_turisticos);
 
-return 0;
+  printf("A cidade %s, localizada no estado %s, possui uma população de %d habitantes, uma área de %.2f km² e um PIB de %.2f bilhões de reais. Com %d pontos turísticos, o código das cartas é %s.\n", 
+           Cidade, Estado, Populacao, Area_km2, Pib, Pontos_turisticos, Codigo_carta);
 
-
+    return 0;
 }
 
 
